8b39cfcf440819ac2f468edf71272dc34ba4e110
Gleb <gleborlov20022002@gmail.com>
2023-08-14 17:15:14 +0300

    added mocks

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,25 +3,25 @@ import EventListView from '../view/events-list-view.js';
 import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 import { render } from '../render.js';
-
-const POINT_COUNT = 3;
-
 export default class BoardPresenter{
   sortComponent = new SortView();
   eventListComponent = new EventListView();
 
-  constructor(container){
+  constructor(container, pointsModel){
     this.container = container;
+    this.pointsModel = pointsModel;
   }
 
   init(){
+    this.boardPoints = [...this.pointsModel.getPoints()];
+
     render(this.sortComponent, this.container);
     render(this.eventListComponent, this.container);
 
-    render(new PointEditView(), this.eventListComponent.getElement());
+    render(new PointEditView(this.boardPoints[0]), this.eventListComponent.getElement());
 
-    for(let i = 0; i < POINT_COUNT; i++){
-      render(new PointView(), this.eventListComponent.getElement());
+    for(let i = 1; i < this.boardPoints.length; i++){
+      render(new PointView(this.boardPoints[i]), this.eventListComponent.getElement());
     }
   }
 }
--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -1,9 +1,21 @@
 import { createElement } from '../render.js';
-import { createTripInfoView } from '../template/trip-info-template.js';
 
+function createTripInfoTemplate(){
+  return(`<section class="trip-main__trip-info  trip-info">
+  <div class="trip-info__main">
+    <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+
+    <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+  </div>
+
+  <p class="trip-info__cost">
+    Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+  </p>
+</section>`);
+}
 export default class TripInfoView {
   getTemplate() {
-    return createTripInfoView();
+    return createTripInfoTemplate();
   }
 
   getElement() {
